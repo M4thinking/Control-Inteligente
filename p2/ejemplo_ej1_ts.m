5452e63762f9685ac84ed35ef1147337ba87fdba
m4thinking <sebastianguzman@ug.uchile.cl>
2023-04-21 10:21:01 -0400

    🔨 Se agrega predicción a 8 y 16 pasos preliminar

--- p2/ejemplo_ej1_ts.m
+++ p2/ejemplo_ej1_ts.m
@@ -34,19 +34,10 @@ max_clusters = 16;
 
 porcentajes=[0.6,0.2,0.2];
 [y ,x]=autoregresores(out.entrada,out.salida,max_regs);
-[Y_val , Y_test, Y_ent, X_val, X_test, X_ent] = separar_datos(y, x, porcentajes);
+[Y_test , Y_val, Y_ent, X_test, X_val, X_ent] = separar_datos(y, x, porcentajes);
 %% Optimizar modelo - Reglas
-[err_test, err_ent] = clusters_optimo(Y_test, Y_ent, X_test, X_ent, max_clusters);
-% Criterio de Akaike
-% n = length(Y_test); % Tamaño de la muestra
-% AIC = [];
-% for k=1:max_clusters-1
-%     err = err_test(k); % Error de prueba (tomando el último valor en err_test)
-%     AIC = [AIC, n*(1 + log(err)) + 2*k + (2*k*(k+1))./(n-k-1)]; % Fórmula del criterio de Akaike
-%     [min_AIC, idx] = min(AIC); % Busca el valor mínimo de AIC y su índice correspondiente
-%     best_clusters = idx; % El mejor número de clusters es el índice con el valor mínimo de AIC
-% end
-best_clusters = 2;
+[err_test, err_ent] = clusters_optimo(Y_val, Y_ent, X_val, X_ent, max_clusters);
+best_clusters = 2; % Criterio del codo
 figure()
 plot(err_test, 'b')
 hold on
@@ -55,12 +46,16 @@ legend('Error de test', 'Error de entrenamiento')
 
 %% Optimizar modelo - Regresores
 [p, indices] = sensibilidad(Y_ent, X_ent, best_clusters); % 2 es el numero de clusters elegido anteriormente
-% Elijo los regresores más importantes a partir del análisis anterior
 n_regresores = 4; % Cambiar valor para mayor o menor número de regresores
-[~, sorted_indices] = sort(indices, 'descend');
-x_optim_ent = X_ent(:, sort(sorted_indices(1:n_regresores), 'ascend'));
-x_optim_val = X_val(:, sort(sorted_indices(1:n_regresores), 'ascend'));
+best_indices = [];
+for i=1:n_regresores % Descartamos peor regresor
+    [~, idx] = max(indices);
+    best_indices = [best_indices, idx];
+    indices(idx) = 0;
+end
 
+x_optim_ent = X_ent(:, sort(best_indices, 'ascend'));
+x_optim_val = X_val(:, sort(best_indices, 'ascend'));
 
 %% Entrenar modelo
 [model, ~] = TakagiSugeno(Y_ent, x_optim_ent, best_clusters, [1 2 2]);
@@ -101,7 +96,7 @@ end
 
 t = 1:length(Y_ent);
 len = 1000; % Cambiar a length(Y_ent) para ver todos los datos.
-
+figure();
 % Graficar intervalos de incertidumbre
 for i = flip(1:9)
     alpha = alphas(i);
@@ -114,7 +109,7 @@ for i = flip(1:9)
 end
 
 % Graficar curva de estimación y_hat_ent
-plot(t(1:len), y_hat_ent(1:len), 'r-', 'LineWidth', 1);
+plot(t(1:len), y_hat(1:len), 'r-', 'LineWidth', 1);
 hold on;
 % Graficar puntos Y_ent
 scatter(t(1:len), Y_ent(1:len), 5, 'b', 'filled');
@@ -125,3 +120,45 @@ ylabel('Variable dependiente');
 title('Modelo con intervalo de incertidumbre - Método de la covarianza');
 legend('90%','80%','70%', '60%','50%', '40%','30%','20%','10%',...
     'Estimación', 'Datos de entrenamiento', 'Intervalos de incertidumbre');
+
+%% Evaluar predicciones a 8 y 16 pasos
+regs = 2;
+% Predicciones a 8 pasos con forloop para ysim. x_optim_val = [yk-1, yk-2, ..., yk-Ny, uk-1, uk-2, ..., uk-Nu] 
+uk = x_optim_val(:, regs+1); % uk = [uk-1, uk-2]
+yk = zeros(length(x_optim_val), 16); % [yk, yk+1, ..., yk+7]
+% Evaluar 8 veces el modelo para obtener predicciones hasta yk+7|k-1 y luego hasta yk+15|k-1
+
+[y, x_optim_val_pred] = autoregresores(uk, Y_val, regs); % x_optim_val_pred = [yk-1, yk-2, uk-1, uk-2]
+for i=1:16
+    disp(i);
+    % yk = [zeros; ysim]
+    yy = [zeros(regs, 1); ysim(x_optim_val_pred, model.a, model.b, model.g)]; % yk = [yk, yk+1, ..., yk+7]
+    yk(:, i) = yy;
+    % Ponemos que en uk, uk+1, ..., uk+7 sean ceros
+    [y, x_optim_val_pred] = autoregresores([zeros(i, 1); uk(1:end-i)],yk(:, i), regs); % x_optim_val_pred = [yk-1, yk-2, uk-1, uk-2]
+end
+
+y_hat_8 = yk(1:end-8, 8); % Predicciones a 8 pasos
+y_hat_16 = yk(1:end-16, 16); % Predicciones a 16 pasos
+
+ 
+%%
+% Graficar predicciones a 8 y sus intervalos de incertidumbre
+figure()
+plot(Y_val, '.b')
+hold on;
+% fill y_hat_8_sup y y_hat_8_inf
+% t = 1:length(Y_val);
+% t2 = [t, fliplr(t)];
+% inBetween = [y_hat_8_sup; flipud(y_hat_8_inf)];
+% fill(t2, inBetween, [0.5 0.5 1], 'FaceAlpha', 0.5);
+% hold on;
+plot(y_hat_8, 'r')
+hold on;
+plot(y_hat_16, 'g')
+legend('Valor real', 'y_hat_8', 'y_hat_16')
+ 
+
+ 
+
+ 
